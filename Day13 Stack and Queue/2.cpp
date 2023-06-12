98ed7ab191205024740fc31ef4c36976646c96f5
Kundan <kundan51kk@gmail.com>
2023-06-13 04:33:47 +0530

    First Commit

---
+++ Day13 Stack and Queue/2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#define ll long long int
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int T=1;
+  // cin>>T;
+  while (T--)
+  {
+    cout<<"Hellow World";
+  }
+  return 0;
+}
---
+++ Day13 Stack and Queue/one.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Stack {
+  int size;
+  int * arr;
+  int top;
+  public: 
+  Stack(){
+    top=-1;
+    size=1000;
+    arr=new int[size];
+  }
+  void push(int x){
+    top++;
+    arr[top]=x;
+  }
+   int pop(){
+    int x=arr[top];
+    top--;
+    return x;
+  }
+  int Top(){
+    return arr[top];
+  }
+  int Size(){
+    return top+1;
+  }
+
+};
+
+int main(){
+  Stack s;
+  s.push(6);
+  s.push(3);
+  s.push(7);
+  cout<<s.Top()<<endl;
+  cout<<s.Size()<<endl;
+  cout<<s.pop()<<endl;
+  cout<<s.Size()<<endl;
+  cout<<s.Top()<<endl;
+  return 0;
+}
