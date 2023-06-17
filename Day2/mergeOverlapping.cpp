b22f3dd76a3ab9dc52041da84aa53a954ff2def2
Kundan <kundan51kk@gmail.com>
2023-06-17 15:19:57 +0530

    added some more sol.

---
+++ Day2/mergeOverlapping.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void mergeOverlap(vector<pair<int,int>> &vp{
+  int n = vp.size();
+  vector<pair<int,int>> ans;
+  for(int i=0;i<n;i++){
+    if(vp[i].second>vp[i+1].first)
+  }
+}
